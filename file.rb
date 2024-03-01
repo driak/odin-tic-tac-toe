fd3e8d08ca6a61e155fb696af55adad6dd891b38
boknik <154282366+driak@users.noreply.github.com>
2024-03-02 00:16:16 +0200

    add turn flow logic

--- file.rb
+++ file.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 grid = [
-         ['o', '', ''],
-         ['', 'o', ''],
-         ['', '', 'o']
+         ['', '', ''],
+         ['', '', ''],
+         ['', '', '']
        ]
 
 grid.each { |row| p row }
@@ -32,7 +32,7 @@ def diagonals(grid)
 
   3.times do |index|
     left_diagonal << rows(grid)[index][index]
-    right_diagonal << flip(grid)[index][index]
+    right_diagonal << rows(flip(grid))[index][index]
   end
 
   [left_diagonal, right_diagonal]
@@ -61,4 +61,29 @@ def same_successive_triple_marker?(grid)
   diagonal_triple_succession?(grid)
 end
 
-puts same_successive_triple_marker?(grid)
+def win?(grid)
+  same_successive_triple_marker?(grid)
+end
+
+turn_count = 0
+
+until win?(grid)
+  marker = turn_count.even? ? 'x' : 'o'
+  indices = ''
+  loop do
+    puts 'input row and column index consecutively (for example 02 -- here, 0 is the row index, 2 is the column index)'
+    input = gets.chomp.chars.map(&:to_i)
+
+    if input.length == 2 && input.all? { |char| (0..2).include?(char) } && grid[input[0]][input[1]] == ''
+      indices = input
+      break
+    end
+  end
+
+  row_index, column_index = indices
+  grid[row_index][column_index] = marker
+
+  grid.each { |row| p row }
+
+  turn_count += 1
+end
